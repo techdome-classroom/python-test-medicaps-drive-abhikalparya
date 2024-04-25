a6c106e04f22d8815c35e874d48b9e77db5ddc57
Abhikalp Arya <abhikalp.arya@gmail.com>
2024-04-25 13:35:27 +0530

    completed prog 1 and 2

--- program1.py
+++ program1.py
@@ -1,10 +1,17 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        stack = []
+        map = {")": "(", "}": "{", "]": "["}
+    
+        for char in s:
+            if char in map:
+                top = stack.pop() if stack else '#'
+                if map[char] != top:
+                    return False
+            else:
+                stack.append(char)
+    
+        return not stack
     
 
 
